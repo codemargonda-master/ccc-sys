aa89388b4165b2f92eca4c5195c4b8a130fed6b5
M Haidar Hanif <git@mhaidarhanif.com>
2017-05-16 15:50:29 +0700

    Change var to const

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,15 +1,15 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var mysql = require('mysql')
+const express = require('express')
+const bodyParser = require('body-parser')
+const mysql = require('mysql')
 
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true})) // support x-www-form-urlencoded
 app.use(bodyParser.json())
 
-var connection = mysql.createConnection({host: 'localhost', user: 'root', password: '', database: 'rest-crud-node'})
+const connection = mysql.createConnection({host: 'localhost', user: 'root', password: '', database: 'ccc_system'})
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (!err) {
     console.log('Database is connected ...')
   } else {
@@ -17,15 +17,33 @@ connection.connect(function(err) {
   }
 })
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send({msg: 'Hello from server!'})
 })
 
+app.get('/get', function (req, res) {
+  const query = "SELECT * FROM user WHERE name='alfon' "
+
+  connection.query(query, function (err, rows) {
+    if (err) { throw err }
+    res.json({success: true, msg: 'Success'})
+  })
+})
 // post data to DB | POST (register)
-app.post('/auth', function(req, res) {
+app.post('/auth', function (req, res) {
+  // // validation
+  // req.assert('name', 'Name is required').notEmpty();
+  // req.assert('email', 'A valid email is required').isEmail();
+  // req.assert('password', 'Enter a password 6 - 20').len(6, 20);
+  //
+  // var errors = req.validationErrors();
+  // if (errors) {
+  //   res.status(422).json(errors);
+  //   return;
+  // }
 
   // get data
-  var data = {
+  const data = {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password
@@ -33,9 +51,9 @@ app.post('/auth', function(req, res) {
 
   var query = 'SELECT count(*) as count FROM t_user WHERE email= ' + mysql.escape(data.email)
 
-  connection.query(query, function(error, rows) {
+  connection.query(query, function (error, rows) {
     if (rows[0].count != 1) {
-      connection.query('INSERT INTO t_user set ? ', data, function(error, rows) {
+      connection.query('INSERT INTO t_user set ? ', data, function (error, rows) {
         if (error) {
           res.send(error)
         }
@@ -49,19 +67,17 @@ app.post('/auth', function(req, res) {
       res.send(error)
     }
   })
-
 })
 
-// login authentication
-app.post('/signin', function(req, res) {
+app.post('/signin', function (req, res) {
   var data = {
     email: req.body.email,
     password: req.body.password
   }
 
-  var query = 'SELECT *,count(*) as count FROM t_user WHERE email= ' + mysql.escape(data.email) + ' AND password= ' + mysql.escape(data.password) + ''
+  var query = 'SELECT * FROM user WHERE email= ' + mysql.escape(data.email) + ' AND password= ' + mysql.escape(data.password) + ''
 
-  connection.query(query, function(error, rows, fields) {
+  connection.query(query, function (error, rows, fields) {
     if (rows[0].count != 1) {
       res.status(401).json({success: 'false', msg: 'Data invalid'})
       console.log('Data invalid!')
@@ -72,7 +88,6 @@ app.post('/signin', function(req, res) {
       res.send(error)
     }
   })
-
 })
 
 module.exports = app
